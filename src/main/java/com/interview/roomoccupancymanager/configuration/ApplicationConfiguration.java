d81ca07ba177fdc50e5db983076aa26c5916124d
Zoltan Szabo <xfire1995@gmail.com>
2022-10-19 19:31:06 +0200

    Spring Boot initial commit
    Signed-off-by: Zoltan Szabo <xfire1995@gmail.com>

---
+++ src/main/java/com/interview/roomoccupancymanager/configuration/ApplicationConfiguration.java
@@ -0,0 +1,7 @@
+package com.interview.roomoccupancymanager.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+}
