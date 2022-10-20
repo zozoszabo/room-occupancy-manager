c66974c2dbdd1c8a247c39086b5f7d507e509f93
Zoltan Szabo <xfire1995@gmail.com>
2022-10-20 07:45:43 +0200

    Enable Swagger API documentation
    Signed-off-by: Zoltan Szabo <xfire1995@gmail.com>

--- src/main/java/com/interview/roomoccupancymanager/configuration/ApplicationConfiguration.java
+++ src/main/java/com/interview/roomoccupancymanager/configuration/ApplicationConfiguration.java
@@ -1,10 +1,31 @@
 package com.interview.roomoccupancymanager.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 @ConfigurationPropertiesScan(basePackages = "com.interview.roomoccupancymanager.configuration.value")
 public class ApplicationConfiguration {
+
+    @Bean
+    public Docket getDocker(@Value("${room-occupancy-manager.name}") final String name,
+                            @Value("${room-occupancy-manager.version}") final String version) {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.interview.roomoccupancymanager.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder().version(version).title(name).build());
+    }
 
 }
